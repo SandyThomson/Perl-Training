88889c77f045cbe5726d2ba8a6487867c657bb65
Robert Main <robert.main@datacash.com>
2014-06-19 14:44:36 +0100

    ram's answers

---
+++ chapter2/ram/exercise_1.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my $pi = 3.141592654;
+my $radius = 12.5;
+my $circumference = 2 * $pi * $radius;
+
+# Display the result
+print "Circumference for circle with radius [$radius] is [$circumference].\n";
