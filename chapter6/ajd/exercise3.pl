80444ae6957d2cbe021a69af7f8a584827a5e7fa
Alex Deas <alex.deas@datacash.com>
2014-07-02 13:55:27 +0100

    Adding exercise 3

---
+++ chapter6/ajd/exercise3.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+# First, find out which environment variable has the longest name
+my $width = 0;
+
+foreach my $key ( keys %ENV ) {
+  if ( length $key > $width ) {
+    $width = length $key;
+  }
+}
+
+# Print the keys and values in a nice format
+printf ( "%-${width}s %s\n", 'Variable', 'Value' );
+foreach my $key ( sort keys %ENV ) {
+  printf ( "%-${width}s %s\n", $key, $ENV{$key} );
+}
