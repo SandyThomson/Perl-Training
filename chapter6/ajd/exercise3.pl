9a8d822154bfec6683159f94c35d0259d905c1ab
Alex Deas <alex.deas@datacash.com>
2014-07-02 14:06:34 +0100

    Updated exercise 3

--- chapter6/ajd/exercise3.pl
+++ chapter6/ajd/exercise3.pl
@@ -6,6 +6,7 @@ use warnings;
 # First, find out which environment variable has the longest name
 my $width = 0;
 
+# Walk the array to find the length of the longest element
 foreach my $key ( keys %ENV ) {
   if ( length $key > $width ) {
     $width = length $key;
