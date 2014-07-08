38742e085eeea6c6b01d6da6dbdbc57809186012
Alex Deas <alex.deas@datacash.com>
2014-07-08 10:09:12 +0100

    Condensing the exercises for this chapter into one script

---
+++ chapter7/ajd/chapter7.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+if (! scalar @ARGV ) {
+  print "Enter some lines, Ctrl-D to exit:\n";
+}
+
+# Keeping line breaks because lazy
+my @lines = <>;
+
+# Set up all the regexes for this chapter
+my %exercises = (
+    1 => { regex => qr/fred/, description => 'fred' },
+    1.1 => { regex => qr/\bfred\b/, description => 'fred (standalone)' },
+    2 => { regex => qr/[Ff]red/, description => 'fred or Fred' },
+    2.1 => { regex => qr/\b[Ff]red/, description => 'fred or Fred (standalone)' },
+    3 => { regex => qr/\./, description => 'a full stop' },
+    4 => { regex => qr/\b[A-Z]\w*[a-z]+\w*/, description => "a capitalised word that isn't all capitalised" },
+    5 => { regex => qr/(\w)\1/, description => 'a double letter' },
+    6 => { regex => qr/(?:fred.*wilma|wilma.*fred)/, description => 'fred and wilma' },
+    );
+
+# Check each of them against the input lines
+foreach my $exercise (sort keys %exercises) {
+  printf ( "\nExercise %s:\n", $exercise );
+  printf ( "\nLines Containing: %s\n", $exercises{$exercise}->{description} );;
+  print grep( /$exercises{$exercise}->{regex}/, @lines );
+}
+
+
+
