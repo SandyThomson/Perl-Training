7a1148adfb5ae9b97eadcb88292d17d03d828970
Alex Deas <alex.deas@datacash.com>
2014-07-16 15:42:45 +0100

    Adding exercise 3
    Included some sample text to test it

---
+++ chapter9/ajd/exercise3.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+# Need a file
+if ( !defined $ARGV[0] || !-s $ARGV[0] ) {
+  die( "Specify a file\n" );
+}
+
+# Backup to a tmp file
+$^I = '.tmp';
+
+# Replace
+while ( <> ) {
+  s{(fred|wilma)}{($1 =~ /fred/i) ? 'Wilma' : 'Fred'}ige;
+  print;
+}
+
+# Sounded like the resulting file should be .out rather than
+# the backup. Stealing rename from Chapter 13 since they mentioned it.
+rename ("$ARGV", "$ARGV.out");
+rename ("$ARGV.tmp", "$ARGV");
