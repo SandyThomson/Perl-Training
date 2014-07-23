f851fd40389b71e2d781f20bba312f49f84a790e
Alex Deas <alex.deas@datacash.com>
2014-07-23 12:05:26 +0100

    Adding exercise 2

---
+++ chapter11/ajd/exercise2.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use DateTime;
+
+if ( scalar @ARGV < 3 ) {
+  die "Usage: $0 <year> <month> <day>\n";
+}
+
+my %date_fields = (
+    year => $ARGV[0],
+    month => $ARGV[1],
+    day => $ARGV[2],
+  );
+
+my $current = DateTime->from_epoch( epoch => time );
+my $user_defined = DateTime->new( \%date_fields );
+
+my $difference = $current - $user_defined;
+
+# Deal with dates in the future
+if ( $difference->is_negative() ) {
+  $difference = $difference->inverse();
+}
+
+my @units = $difference->in_units( qw/years months days/ );
+
+printf( "%d years, %d months, and %d days\n", @units );
+
