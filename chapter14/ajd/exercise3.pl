a1e9a160ba414ace79f2747cbd9d60704b0927e5
Alex Deas <alex.deas@datacash.com>
2014-08-06 15:32:54 +0100

    Stashing exercise 3

---
+++ chapter14/ajd/exercise3.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Getopt::Long;
+use File::Slurp;
+
+# Find the indexes of occurences of a string within a larger string
+
+# Where is the large string coming from? File? stdin?
+# Same with the small string
+
