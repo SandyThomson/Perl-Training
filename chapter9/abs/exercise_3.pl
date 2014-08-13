d51b702435766e996684a6f205d47a6a227ec566
arranstobbs <arranstobs@gmail.com>
2014-08-13 19:11:10 +0000

    completed exercise 3

--- chapter9/abs/exercise_3.pl
+++ chapter9/abs/exercise_3.pl
@@ -5,8 +5,17 @@ use warnings;
 
 $^I = '.out';
 
+if ( !@ARGV ) {
+	die "Usage: ./exercise_2.pl <FILE_NAMES>";
+}
+
 while (<>) {
-	s/Fred/Wilma/i;
+	# split on \n, so lets use this as a
+	# placeholder for what will be 'Wilma'
+	s/Fred/\n/i;
+	# replace wilma with fred
 	s/Wilma/Fred/i;
+	# update placeholders
+	s/\n/Wilma/;
 	print;
 }
