5edec63b75e502720fedd41eb02baa94dfecb36f
ardavey <gr00ved@gmail.com>
2014-08-21 00:41:19 +0100

    Unborked ex4

--- chapter9/ard/ex4.pl
+++ chapter9/ard/ex4.pl
@@ -6,7 +6,10 @@ $^I = '.bak';
 my $copyright = '## Copyright (C) 2014 by ard';
 
 while ( <> ) {
-  if ( /^#!.*$/ ) {
-    print "$&\n$copyright";
+  if ( /^#!.*$/s ) {
+    print "$&$copyright\n";
+  }
+  else {
+    print;
   }
 }
