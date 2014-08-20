c70ee849ff305c890d91a2e8b08ea50d60cc5236
Mathieu Patureau <mathieu.patureau@gmail.com>
2014-08-20 22:49:21 +0100

    Add chapter 10

---
+++ chapter10/mpm/2.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use feature qw( say);
+# No sure what they want me to change this time ??!
+
+my $secret = int(1 + rand 100);
+
+
+while(<>) {
+	chomp;
+	if ( $_ eq "quit" || 
+		 $_ eq "exit" ||
+		 $_ eq "" ||
+		 $_ eq $secret
+		 ) {
+		exit;
+	}
+	
+	if( $_ eq  "cheat" ){
+		say "pss... the number is $secret" ;
+		next;
+	}
+	
+	
+	if( $_ > $secret){
+		say "Too high" ;
+		next;
+	}
+	
+	if( $_ < $secret){
+		say "Too low" ;
+	}
+	
+}
