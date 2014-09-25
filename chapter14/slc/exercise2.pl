60b0b80c56792e2e1f045c86981e38b6a2c7bd30
Sean Clark <sean.clark84@googlemail.com>
2014-09-25 22:30:40 +0100

    Update exercise2.pl
    removed comment

--- chapter14/slc/exercise2.pl
+++ chapter14/slc/exercise2.pl
@@ -5,5 +5,4 @@ use 5.010;
 
 my %last_name = qw ( fred flintstone Wilma Flintstone Barney Rubble betty rubble Bamm-Bamm Rubble PEBBLES FLINTSTONE );
 
-# sort, then map each as a 20 character right justified string and join with a newline
 say join "\n", map { sprintf "%s %s", $_, $last_name{$_}} sort { "\L$last_name{$a}" cmp "\L$last_name{$b}" or "\L$a" cmp "\L$b" } keys %last_name;
