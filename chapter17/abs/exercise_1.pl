140fd1ad2ad4dc6d55cdb65b8ea2fce28976f44b
Arran Stobbs <arranstobbs@gmail.com>
2014-10-15 22:43:43 +0100

    Updated to use Try::Tiny

--- chapter17/abs/exercise_1.pl
+++ chapter17/abs/exercise_1.pl
@@ -3,19 +3,30 @@
 use strict;
 use warnings;
 
+use Try::Tiny;
+
 use feature qw( say );
 
 my @lines = <>;
 
 say( "Enter Pattern?" );
-while( my $pattern = <STDIN> ne '' ) {
+chomp( my $pattern = <STDIN> );
+
+while( $pattern ne '' ) {
+	my @matches = ();
+	try {
+		chomp( @matches = grep { $_ =~ $pattern } @lines );
+	} catch {
+		say( "Bad RegEx: $_" );
+	} finally {
+		if ( scalar( @matches ) == 0 ) {
+			say( 'No Matches' );
+		} else {
+			my $num_of_matches = @matches;
+			say( "Number of matches: $num_of_matches\n-" . join( "\n-", @matches ) );
+		}
+	};
+
 	say( "Enter Pattern?" );
-	chomp( $pattern );
-	chomp( my @matches = eval { grep { $_ =~ $pattern } @lines } );
-	if ( $@ ) {
-		say( "Bad RegEx: $@" );
-	} else {
-		my $num_of_matches = @matches;
-		say( "Number of matches: $num_of_matches\n-" . join( "\n-", @matches ) );
-	}
+	chomp( $pattern = <STDIN> );
 }
